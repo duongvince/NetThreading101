aa4d5efb56648ba9dda8f3c6760ab772906775bc
duongvince <duongvince@hotmail.com>
2016-04-20 12:13:28 -0700

    Test

---
+++ example1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace example1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRed
+            // 
+            this.btnRed.Location = new System.Drawing.Point(13, 13);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(75, 23);
+            this.btnRed.TabIndex = 0;
+            this.btnRed.Text = "Red";
+            this.btnRed.UseVisualStyleBackColor = true;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.Location = new System.Drawing.Point(95, 12);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(75, 23);
+            this.btnBlue.TabIndex = 1;
+            this.btnBlue.Text = "Blue";
+            this.btnBlue.UseVisualStyleBackColor = true;
+            this.btnBlue.Click += new System.EventHandler(this.btnBlue_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 114);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnRed);
+            this.Name = "frmMain";
+            this.Text = "Draw Rect";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnBlue;
+    }
+}
+
---
+++ example1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+
+namespace example1
+{
+    public partial class frmMain : Form
+    {
+        Random mRdm;
+        Thread mThreadRed;
+        Thread mThreadBlue;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            mThreadRed = new Thread(DrawRedRectangles);
+            mThreadRed.Start();
+
+            // Resume thread:  mThreadBlue.Resume()
+            // Suspend thread:  mThreadRed.Suspend()
+
+        }
+
+        private void btnBlue_Click(object sender, EventArgs e)
+        {
+            mThreadBlue = new Thread(DrawBlueRectangles);
+            mThreadBlue.Start();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            mRdm = new Random();
+            this.Width = 800;
+            this.Height = 500;
+        }
+
+        private void DrawRedRectangles()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(100);
+                this.CreateGraphics().DrawRectangle(new Pen(Brushes.Red, 4), new Rectangle(mRdm.Next(0, this.Width), mRdm.Next(0, this.Height), 20, 20));
+            }
+        }
+
+        private void DrawBlueRectangles()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(100);
+                this.CreateGraphics().DrawRectangle(new Pen(Brushes.Blue, 4), new Rectangle(mRdm.Next(0, this.Width), mRdm.Next(0, this.Height), 20, 20));
+            }
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (mThreadRed != null)
+                mThreadRed.Join();      // .Join() - wait for thread to finish, or .Abort() - terminate thread
+            if (mThreadBlue != null)
+                mThreadBlue.Join();
+            MessageBox.Show("threads done");
+            if ((mThreadRed != null && mThreadRed.IsAlive) || (mThreadBlue != null && mThreadBlue.IsAlive))
+                e.Cancel = true;
+        }
+    }
+}
